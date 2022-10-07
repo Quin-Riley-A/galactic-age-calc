a5fcfb5dcac1c08f963613496723061e5167cb6d
Quin-Riley-A <quin.asselin1@gmail.com>
2022-10-06 21:06:55 -0400

    receive good fail on initial Ages class TDD

---
+++ __tests__/ages.tests.js
@@ -0,0 +1,21 @@
+import Ages from './../src/classes/ages.js'
+
+describe('Ages', () => {
+  test('It should construct an object of the Ages-class producing age-properties from a passed in date-object.', () => {
+    let userBirthday = new Date(95, 0, 1);
+    let currentDate = new Date;
+    let userGalacticAge = new Ages(userBirthday, currentDate);
+    let currentAge = currentDate-userBirthday;
+    expect(userGalacticAge.earthAge).toEqual(currentAge);
+    expect(userGalacticAge.mercuryAge).toEqual(currentAge * 88/365.3);
+    expect(userGalacticAge.venusAge).toEqual(currentAge * 224.7/365.3);
+    expect(userGalacticAge.marsAge).toEqual(currentAge * 687/365.3);
+    expect(userGalacticAge.ceresAge).toEqual(currentAge * 1680/365.3);
+    expect(userGalacticAge.jupiterAge).toEqual(currentAge * 4332.6/365.3);
+    expect(userGalacticAge.saturnAge).toEqual(currentAge * 24491/365.3);
+    expect(userGalacticAge.uranusAge).toEqual(currentAge * 30688.5/365.3);
+    expect(userGalacticAge.neptuneAge).toEqual(currentAge * 60195/365.3);
+    expect(userGalacticAge.plutoAge).toEqual(currentAge * 90560/365.3);
+    expect(userGalacticAge.erisAge).toEqual(currentAge * 204199/365.3);
+  })
+})
