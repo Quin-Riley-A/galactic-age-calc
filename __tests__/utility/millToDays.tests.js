b9e6c2c37dfe3af49d255455a4b3f329828a2161
Quin-Riley-A <quin.asselin1@gmail.com>
2022-10-06 23:53:40 -0400

    produce good fail for second test of millToDays utility function

--- __tests__/utility/millToDays.tests.js
+++ __tests__/utility/millToDays.tests.js
@@ -5,4 +5,13 @@ describe('millToDays', () => {
     const daysResult = millToDays(86400000);
     expect(daysResult).toEqual(1);
   });
+
+  test('It should round a converted number to the nearest hundredth', () => {
+    const daysResult = millToDays(86400050);
+    const daysResult2 = millToDays(43200000);
+    const daysResult3 = millToDays(21600000);
+    expect(daysResult).toEqual(1);
+    expect(daysResult2).toEqual(0.50);
+    expect(daysResult3).toEqual(0.25);
+  });
 });
