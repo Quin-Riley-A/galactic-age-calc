2cbd9b902b178a1b0038c9c1739ed8672409ed5d
Quin-Riley-A <quin.asselin1@gmail.com>
2022-10-09 07:19:32 -0400

    adjust output readablity and add more to output UI

--- src/index.js
+++ src/index.js
@@ -19,14 +19,27 @@ function userSubmit(event) {
   const age1 = new Ages(userDate, currentDate);
   age1.lifeExpectCalc(userExpiryDate*365.242189*24*60*60*1000);
   console.log(age1.estimatedExpiryDate);
-  displayPlanetAges();
+  displayPlanetAges(age1);
 }
 
-function displayPlanetAges() {
-  let outputDiv = document.getElementById('outputDiv');
+function displayPlanetAges(age1) {
+  let outputDiv = document.getElementById("outputDiv");
+  if (outputDiv === null) {
+    outputDiv = document.createElement('div');
+    outputDiv.setAttribute("id", "outputDiv");
+  }
   if (outputDiv !== null) {
     while (outputDiv.firstChild) {
       outputDiv.removeChild(outputDiv.firstChild);
     }
   }
+  const h3 = document.createElement('h3');
+  h3.innerText = "Interplanetary Expiration Date:";
+  outputDiv.append(h3);
+  const birthdayPTag = document.createElement('p');
+  birthdayPTag.innerText = "Your chosen birthdate is " + age1.birthday;
+  outputDiv.append(birthdayPTag);
+  const demisePTag = document.createElement('p');
+  demisePTag.innerText = "Based on your life expectancy, your estimated 'expiration date' is " + age1.estimatedExpiryDate;
+  document.querySelector('body').append(outputDiv);
 }
