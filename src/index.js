a6ccfb4edbab99ea71350cf1f4a1d708bb4746d7
Quin-Riley-A <quin.asselin1@gmail.com>
2022-10-09 05:41:23 -0400

    calculate users expected expiry date based on form input

--- src/index.js
+++ src/index.js
@@ -13,11 +13,13 @@ function winLoad() {
 function userSubmit(event) {
   event.preventDefault();
   let currentDate = new Date();
-  let userDateArr = document.getElementById('userDate').value.split('-');
+  const userDateArr = document.getElementById('userDate').value.split('-');
   const userDate = new Date(userDateArr[0], userDateArr[1]-1, userDateArr[2]);
+  const userExpiryDate = parseFloat(document.getElementById('userLifeExpect').value);
   const age1 = new Ages(userDate, currentDate);
+  age1.lifeExpectCalc(userExpiryDate*365.242189*24*60*60*1000);
+  console.log(age1.estimatedExpiryDate);
   displayPlanetAges();
-  console.log("current Age: " + age1);
 }
 
 function displayPlanetAges() {
