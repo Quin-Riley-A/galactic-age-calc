417e7fc05ecb825a5c976508c21d992a5ca3c0d8
Quin-Riley-A <quin.asselin1@gmail.com>
2022-10-09 01:16:40 -0400

    change function name from assignAge to ageAssign

--- src/utility/ageAssign.js
+++ src/utility/ageAssign.js
@@ -1,4 +1,4 @@
-export default function assignAge(index, ageInDays) {
+export default function ageAssign(index, ageInDays) {
   const planetYearLen = [87.97 + 'merYear', 224.7 + 'venYear', 365.25 + 'earYear', 687.98 + 'marYear', 1680 + 'cerYear', 4332.59 + 'jupYear', 10759.22 + 'satYear', 30688.5 + 'uraYear', 60195 + 'nepYear', 90560 + 'pluYear', 204199 + 'eriYear'];
   return (Math.round(ageInDays*10000/parseFloat(planetYearLen[index])))/10000;
 }
