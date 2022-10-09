07ff81c25d5e325d4611f8cc7db854253e42ea97
Quin-Riley-A <quin.asselin1@gmail.com>
2022-10-09 00:36:17 -0400

    construct basic header for testing build in console.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import Ages from './classes/ages.js';
 import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css';
+import './../css/bootstrap.css';
+import './../css/styles.css';
+
+let date1 = new Date(2022, 9, 10);
+let date2 = new Date(2022, 9, 9);
+let age = new Ages(date2, date1);
+console.log(age);
+console.log(age.merAge)
